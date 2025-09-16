db808a2749c724f61f777d0e55d4f8a713c7994f
devashmitha1308 <devashmitha1308@gmail.com>
2025-09-16 22:01:31 +0530

    Add files via upload

---
+++ DSA_Assignment_3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class DSA_Assignment_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[][] arr = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = M - 1; j >= 0; j--) {
+                System.out.print(arr[i][j]);
+                if (j > 0) System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
